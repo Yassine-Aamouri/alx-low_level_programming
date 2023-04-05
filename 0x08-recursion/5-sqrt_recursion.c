55007e2c0d1df976b766f31c7873851d9bb04ccb
Yassine-Aamouri <yassin9aamouri@gmail.com>
2023-04-05 07:07:28 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,43 @@
 #include "main.h"
 
 /**
- *  _pow_recursion - calculates value of x raised to the power of y
- * @x: given number
- * @y: exponent
- * Return: value of x raised to the power of y
+ * sqtRecursive - computes square root recursively
+ * doing binary search
+ * @start: inital number
+ * @end: last number within limit of number
+ * @m: given number
+ * Return: 1 if sqrroot not found, else sqrroot
  */
 
-int _pow_recursion(int x, int y)
+int sqtRecursive(int start, int end, int m)
 {
-	if (y < 0)
+	long mid;
+
+	if (end >= start)
 	{
+		mid = start + (end - start) / 2;
+		if (mid * mid == m)
+			return (mid);
+/* following binary search */
+		if (mid * mid > m)
+			return (sqtRecursive(start, mid - 1, m));
+		if (mid * mid < m)
+			return (sqtRecursive(mid + 1, end, m));
+	}
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: given number
+ * Return: square root of n or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
 		return (-1);
-	}
-	if (y == 0)
-	{
-		return (1);
-	}
-	return (x * _pow_recursion(x, y - 1));
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqtRecursive(2, n, n));
 }
