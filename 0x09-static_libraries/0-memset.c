4f1b8f90e34083b8a61f70ea40ac4101ceb5c869
Yassine-Aamouri <yassin9aamouri@gmail.com>
2023-04-07 07:39:05 -0700

    0x09. C - Static libraries

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,19 +1,21 @@
 #include "main.h"
+
 /**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
- *
- * Return: changed array with new value for n bytes
+ * _memset - fill memory with a constant byte
+ * @s: Buffer string
+ * @b: Constant byte
+ * @n: First n bytes
+ * Return: Pointer to a memory area of s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (; n > 0; i++)
+	while (n > 0)
 	{
 		s[i] = b;
+		i++;
 		n--;
 	}
 	return (s);
